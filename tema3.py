fb6d01e47016c60826fdd039acc5dc3bbe07b67a
Alex <petrachealex2002v@gmail.com>
2022-04-16 11:54:45 +0300

    Tema 3

--- tema3.py
+++ tema3.py
@@ -2,42 +2,32 @@ import json
 import csv
 import uuid
 
-id1 = uuid.uuid4()
-id2 = uuid.uuid4()
-id3 = uuid.uuid4()
-id4 = uuid.uuid4()
-id5 = uuid.uuid4()
-id6 = uuid.uuid4()
-id7 = uuid.uuid4()
-id8 = uuid.uuid4()
-id9 = uuid.uuid4()
-
-cars = [{'id': id(id1),
+cars = [{'id': id(uuid.uuid4),
          'brand': 'Toyota',
          'model': 'hatchback',
          'hp': 76,
          'price': 400},
-        {'id': id(id2),
+        {'id': id(uuid.uuid4),
          'brand': 'Toyota',
          'model': 'suv',
          'hp': 99,
          'price': 600},
-        {'id': id(id3),
+        {'id': id(uuid.uuid4),
          'brand': 'Mazda',
          'model': 'sedan',
          'hp': 88,
          'price': 800},
-        {'id': id(id4),
+        {'id': id(uuid.uuid4),
          'brand': 'Peugeot',
          'model': 'universal',
          'hp': 150,
          'price': 750},
-        {'id': id(id5),
+        {'id': id(uuid.uuid4),
          'brand': 'Peugeot',
          'model': 'roadster',
          'hp': 116,
          'price': 870},
-        {'id': id(id6),
+        {'id': id(uuid.uuid4),
          'brand': 'VW Passat',
          'model': 'sedan',
          'hp': 120,
@@ -47,30 +37,22 @@ cars = [{'id': id(id1),
          'model': 'coupe',
          'hp': 180,
          'price': 1580},
-        {'id': id(id7),
+        {'id': id(uuid.uuid4),
          'brand': 'VW Passat',
          'model': 'coupe',
          'hp': 220,
          'price': 3024},
-        {'id': id(id8),
+        {'id': id(uuid.uuid4),
          'brand': 'Mazda',
          'model': 'coupe',
          'hp': 250,
          'price': 5400},
-        {'id': id(id9),
+        {'id': id(uuid.uuid4),
          'brand': 'Mazda',
          'model': 'targa',
          'hp': 280,
          'price': 5900}, ]
 
-
-slow_cars = []
-fast_cars = []
-sport_cars = []
-
-cheap_car = []
-medium_car = []
-expensive_car = []
 
 toyota_brand_car = []
 peugeot_brand_car = []
@@ -78,27 +60,26 @@ mazda_brand_car = []
 vw_passat_brand_car = []
 
 
+slow_cars = [car for car in cars if car['hp'] < 120]
+slow_cars = sorted(slow_cars, key=lambda x: x['hp'])
+
+fast_cars = [car for car in cars if 120 <= car['hp'] < 180]
+fast_cars = sorted(fast_cars, key=lambda x: x['hp'])
+
+sport_cars = [car for car in cars if car['hp'] >= 180]
+sport_cars = sorted(sport_cars, key=lambda x: x['hp'])
+
+
+cheap_car = [car for car in cars if car['price'] < 1000]
+cheap_car = sorted(cheap_car, key=lambda x: x['price'])
+
+medium_car = [car for car in cars if 1000 <= car['price'] < 5000]
+medium_car = sorted( medium_car, key=lambda x: x['price'])
+
+expensive_car = [car for car in cars if car['price'] >= 5000]
+expensive_car = sorted( expensive_car, key=lambda x: x['price'])
+
 for car in cars:
-    if car['hp'] < 120:
-        slow_cars.append(car)
-        slow_cars = sorted(slow_cars, key=lambda x: x['hp'])
-    elif car['hp'] < 180:
-        fast_cars.append(car)
-        fast_cars = sorted(fast_cars, key=lambda x: x['hp'])
-    elif car['hp'] >= 180:
-        sport_cars.append(car)
-        sport_cars = sorted(sport_cars, key=lambda x: x['hp'])
-
-    if car['price'] < 1000:
-        cheap_car.append(car)
-        cheap_car = sorted(cheap_car, key=lambda x: x['price'])
-    elif car['price'] < 5000:
-        medium_car.append(car)
-        medium_car = sorted(medium_car, key=lambda x: x['price'])
-    elif car['price'] >= 5000:
-        expensive_car.append(car)
-        expensive_car = sorted(expensive_car, key=lambda x: x['price'])
-
     if car['brand'] == 'Toyota':
         toyota_brand_car.append(car)
     elif car['brand'] == 'Peugeot':
@@ -162,3 +143,9 @@ with open('input.csv', 'w') as my_file:
     writer.writerow(keys)
     for car in cars:
         writer.writerow(car.values())
+
+
+
+
+
+
