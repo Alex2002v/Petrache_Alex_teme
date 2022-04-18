b0ad1080866695f3598df8f5a5f244d73f7ec1c1
Alex <petrachealex2002v@gmail.com>
2022-04-18 22:11:23 +0300

    Tema 4

--- Tema4.py
+++ Tema4.py
@@ -1,19 +1,39 @@
-class AlexFraction:
-    def __init__(self, up, down):
-        self.numa = up
-        self.numi = down
+def main():
 
-    def __str__(self):
-        return str(self.numa) + '/' + str(self.numi)
+    def reduced_fraction(x, y):
+        while x % y != 0:
+            xx = x
+            yy = y
+            x = yy
+            y = xx % yy
+        return y
 
-    def __add__(self, another_fraction):
-        numarator = self.numa * another_fraction.numi + self.numi * another_fraction.numa
-        numitor = self.numi + another_fraction.numi
-        return AlexFraction(numarator, numitor)
+    class AlexFraction:
+        def __init__(self, up, down):
+            self.numa = up
+            self.numi = down
+
+        def __str__(self):
+            return str(self.numa) + '/' + str(self.numi)
+
+        def __add__(self, another_fraction):
+            numarator = self.numa * another_fraction.numi + self.numi * another_fraction.numa
+            numitor = self.numi + another_fraction.numi
+            common = reduced_fraction(numarator, numitor)
+            return AlexFraction(numarator // common, numitor // common)
+
+    def inverse(numerator, denominator):
+        return AlexFraction(denominator, numerator)
+
+    class_call1 = AlexFraction(2, 2)
+    class_call2 = AlexFraction(4, 4)
+    the_sum = class_call1 + class_call2
+    print(the_sum)
+
+    str_sum = str(the_sum)
+    list_sum = list(str_sum)
+    print(inverse(list_sum[0], list_sum[2]))
 
 
-class_call1 = AlexFraction(2, 5)
-class_call2 = AlexFraction(4, 6)
-the_sum = class_call1 + class_call2
-print(the_sum)
-
+if __name__ == '__main__':
+    main()
